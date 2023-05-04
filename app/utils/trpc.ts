07436bc0e24a33555196a96ec00560a4526194ce
Jose Toscano <josetoscano@atratopago.com>
2023-05-03 23:12:40 -0600

    Fixed small error in trpc decalration

--- app/utils/trpc.ts
+++ app/utils/trpc.ts
@@ -2,7 +2,7 @@
 
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
-import type { AppRouter } from '@/app/server/api/routers/_app';
+import type { AppRouter } from '@/app/server/api/root';
 
 function getBaseUrl() {
     if (typeof window !== 'undefined')
